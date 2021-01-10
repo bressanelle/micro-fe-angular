fe80376582b9304b6ba432fd7750132f6fffb85e
Hiúry Bressanelle <hiurybressanelle@gmail.com>
2021-01-10 04:26:01 -0300

    feat: render microapp1 and microapp2 in same page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'micro-fe-angular';
+
+  ngOnInit(){
+    const microapp1 = document.createElement('script')
+    microapp1.src = 'http://localhost:8080/microapp1/main.js'
+    document.body.appendChild(microapp1)
+
+    const microapp2 = document.createElement('script')
+    microapp2.src = 'http://localhost:8080/microapp2/main.js'
+    document.body.appendChild(microapp2)
+  }
 }
