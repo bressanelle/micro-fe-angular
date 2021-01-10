eab3e94e3b7d19596f26741bf4e41da6e09dbcd4
Hiúry Bressanelle <hiurybressanelle@gmail.com>
2021-01-10 03:22:10 -0300

    chore: create custom elements for microapps and referece they in root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {  CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent
@@ -13,5 +12,9 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule {
+
+}
